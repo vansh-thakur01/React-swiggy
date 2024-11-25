b185550ab2d3b91df1b904610e7b88c63853fe30
vansh <vansh55dec@gmail.com>
2024-11-25 20:06:53 +0530

    still working on it

--- src/Components/ResMenuCards.js
+++ src/Components/ResMenuCards.js
@@ -1,22 +1,20 @@
 import { MENU_IMG } from "../config/utils.js";
 
 const ResMenuCard = function ({resdata}) {
-  const  a = resdata;
-  console.log(a,"sdsdgdsdgds");
   return (
-    <div className="flex items-center h-[200px] w-[850px] my-6 border-t-2 border-gray-300">
-      <div className="h-[165px] w-[580px]">
+    <div className="flex justify-between items-center h-[200px] w-[1000px] my-6 border-t-2 border-gray-300">
+      <div className="h-[165px] w-[640px]">
         <h4>🟢</h4>
         <h2 className="text-2xl font-bold">{resdata.name}</h2>
         <h4 className="text-xl font-bold"> ₹ {resdata.price / 100}</h4>
         <h4 className="my-2 text-[17px] font-bold">
           <span className="text-green-500">{resdata.ratings.aggregatedRating.rating}</span>
         </h4>
-        <p className="text-[18px] text-gray-500 font-[500]">{resdata.description}</p>
+        <p className="text-[19px] text-gray-500 font-[500]">{resdata.description}</p>
       </div>
       <div>
         <img
-          className="w-[180px] h-[170px] rounded-lg"
+          className="w-[200px] h-[180px] rounded-lg"
           src={
             MENU_IMG +
             resdata.imageId
--- src/Components/RestaurantMenu.js
+++ src/Components/RestaurantMenu.js
@@ -2,37 +2,56 @@ import Shimmer from "./Shimmer-ui.js";
 import { useParams } from "react-router-dom";
 import useRestaurantMenuData from "../config/useRestaurantMenudata.js";
 import ResMenuCard from "./ResMenuCards.js";
+import { useState } from "react";
+import { title } from "process";
 
 const RestaurantMenu = ()=>{   
     const {resid} = useParams();
-  
+    const [cardHidden, setcardHidden] = useState("block");
     const resInfo = useRestaurantMenuData(resid);
-
     if(resInfo === null)  return (
       <div className="mt-4 ml-4">
         <Shimmer />
       </div>
     );
 
-    const {name,cuisines,costForTwoMessage} = resInfo.cards[2].card.card.info;
-    const { itemCards } = resInfo.cards[4].groupedCard.cardGroupMap.REGULAR.cards[2].card.card
-    console.log(itemCards);
-
+    // const {name,cuisines,costForTwoMessage} = resInfo.cards[2].card.card.info;
+    // const { itemCards } = resInfo.cards[4].groupedCard.cardGroupMap.REGULAR.cards[2].card.card
+    // const { card }= resInfo.cards[4].groupedCard.cardGroupMap.REGULAR.cards[2].card;
+    const cardData = resInfo.cards[4].groupedCard.cardGroupMap.REGULAR.cards;
+    console.log(cardData,"opop");
+    const data =  cardData.filter(val=> val?.card?.card?.itemCards)
+    // .forEach((val,i)=>{
+    //    const [`cardHidden${}`,setcardHidden] = useState('block');
+    //   });
     return (
-        <div className="w-[800px] mx-auto">
-            {/* <h1>{name}</h1>
-            <p>{cuisines} - {costForTwoMessage}</p>
-            <h2>Menu</h2>
-            <ul >
-             {itemCards.map((item)=><li key = {item.card.info.id}>{item.card.info.name}</li>)}
-            </ul> */}
-            {itemCards.map(val=>{
-              console.log(val);
-              return <div key={val.card.info.id}><ResMenuCard resdata={val.card.info}/></div>
-              
+      <div>
+        {data.map(val=>(<div className="w-[1000px] mx-auto">
+          <div>
+            <h2
+              className="text-2xl font-extrabold pt-5"
+              onClick={() => {
+                cardHidden === "block"
+                  ? setcardHidden("hidden")
+                  : setcardHidden("block");
+              }}
+            >
+              {val.card.card.title} ({val.card.card.itemCards.length})
+            </h2>
+            <div className={cardHidden}>
+              {val.card.card.itemCards.map((val) => {
+                console.log(val);
+                return (
+                  <div key={val.card.info.id}>
+                    <ResMenuCard resdata={val.card.info} />
+                  </div>
+                );
               })}
-        </div>
-    )
+            </div>
+          </div>
+        </div>))}
+      </div>
+    );
 }
 
 export default RestaurantMenu;
