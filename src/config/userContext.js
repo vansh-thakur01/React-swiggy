86b0d08f7ca036521032a7969cbb0b53e6a54b15
vansh <vansh55dec@gmail.com>
2025-07-08 21:43:50 +0530

    updates

--- src/config/userContext.js
+++ src/config/userContext.js
@@ -1,6 +1,8 @@
 import {createContext} from 'react';
 
-const context = createContext({loginUser:"login"});
+const context = createContext({loginUser:"default"});
+console.log(context,"hhhhh");
+
 export default context;
 
 export const contextForHide = createContext("block");
